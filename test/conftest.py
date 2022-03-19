5fa146804f1159986f556fa791933d673911b968
Osmar Pereira <bibliotecaosmar@gmail.com>
2022-03-19 18:50:14 -0300

    Configuração de testes

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from app import create_app
+
+#
+# Fixtures
+#==============================================
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
