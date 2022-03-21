8dc66dccef39fdbbd6c43a43de690dbd67611be4
Osmar Pereira <bibliotecaosmar@gmail.com>
2022-03-20 21:14:12 -0300

    Continuous integration - 8.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,27 +3,27 @@ import pytest
 from app import create_app
 
 
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update(
-        {
-            "TESTING": True,
-        }
-    )
+# @pytest.fixture()
+# def app():
+#     app = create_app()
+#     app.config.update(
+#         {
+#             "TESTING": True,
+#         }
+#     )
 
-    # other setup can go here
+#     # other setup can go here
 
-    yield app
+#     yield app
 
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
+#     # clean up / reset resources here
 
 
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+# @pytest.fixture()
+# def client(app):
+#     return app.test_client()
+
+
+# @pytest.fixture()
+# def runner(app):
+#     return app.test_cli_runner()
