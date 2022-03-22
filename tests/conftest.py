698123c5775c186f7271c014d061ff6ace638178
Osmar Pereira <bibliotecaosmar@gmail.com>
2022-03-22 08:59:31 -0300

    Atualização prospector

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,42 @@
 import pytest
 
-# from app import create_app
+def create_app():
+    from flask import Flask
+
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    from app.home.controller import home
+    from app.clientes.controllers import clientes
+
+    app.register_blueprint(home)
+    app.register_blueprint(clientes)
+
+    return app
 
 
-# @pytest.fixture()
-# def app():
-#     app = create_app()
-#     app.config.update(
-#         {
-#             "TESTING": True,
-#         }
-#     )
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
 
-#     # other setup can go here
+    # other setup can go here
+    from app.clientes import models
 
-#     yield app
+    yield app
 
-#     # clean up / reset resources here
-
-
-# @pytest.fixture()
-# def client(app):
-#     return app.test_client()
+    # clean up / reset resources here
 
 
-# @pytest.fixture()
-# def runner(app):
-#     return app.test_cli_runner()
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
