0945b2daf3053658dca83f04800f8a90a621194b
Osmar Pereira <bibliotecaosmar@gmail.com>
2022-03-23 17:53:02 -0300

    Application factory - prospector correcão

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash
-# from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 
 from sqlalchemy import create_engine
@@ -9,6 +7,12 @@ from sqlalchemy_utils import create_database, database_exists
 
 
 def create_app():
+    from app.model import db
+    from app.login_manager import lm
+
+    from app.home.controller import home
+    from app.clientes.controllers import clientes
+    
     # Objeto da aplicação flask
     app = Flask(__name__)
 
@@ -16,13 +20,10 @@ def create_app():
     app.config.from_object('config')
 
     # Define banco de dados importados em controllers
-    # db = SQLAlchemy(app)
-    from app.model import db
     db.init_app(app)
 
     migrate = Migrate(app, db)
 
-    from app.login_manager import lm
     lm.init_app(app)
     lm.login_view = 'clientes.login'
 
@@ -38,12 +39,8 @@ def create_app():
         return render_template('404.html'), 404
 
     # IMPORTA TODOS OS BLUEPRINT
-    from app.home.controller import home
-    from app.clientes.controllers import clientes
-
     app.register_blueprint(home)
     app.register_blueprint(clientes)
-
 
     from app.clientes import models
 
